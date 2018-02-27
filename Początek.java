e57e1dbcc0891c0069ffc2df3a4f63857f85428a
Jarek Kijko/kijek1979 <jwkjk@o2.pl>
2018-02-27 07:49:00 +0100

    zmiana nr 5

--- Początek.java
+++ Początek.java
@@ -34,52 +34,75 @@ public class Początek {
 ////
 ////        }
 
-        int[] taqblica = new int[3];
-        int licznik = 0;
-        int licznik2 = 0;
-        boolean flaga = true;
-
-
-           // while (taqblica.length <= 3){
-
-            try {
-                while (licznik <= 2) {
-
-                    System.out.println("podaj liczbe " + (licznik2 + 1));
-                    taqblica[licznik] = scanner.nextInt();
-
-                    licznik++;
-
-
-                }
-            } catch (InputMismatchException ex) {
-                System.out.println(" podałes nieprawidłowy typ liczbowy");
-                System.out.println("pwiedz mi co dalej robić: koniec czy od nowa?");
-                scanner.nextLine();
-                String coDalej = scanner.nextLine();
-                if (coDalej.equals("koniec".toLowerCase().trim())) {
-                    System.out.println("koniec programu");
-                   //break;
-                } else if (coDalej.equals("od nowa".toLowerCase().trim())) {
-
-
-
-
-                }
-
-            }
+//        int[] taqblica = new int[3];
+//        int licznik = 0;
+//        int licznik2 = 0;
+//        //boolean flaga = true;
+//
+//
+//           // while (taqblica.length <= 3){
+//
+//            try {
+//                while (licznik <= 3) {
+//
+//                    System.out.println("podaj liczbe " + licznik2 + 1);
+//                    taqblica[licznik] = scanner.nextInt();
+//                    licznik++;
+//
+//
+//                }
+//            } catch (InputMismatchException ex) {
+//                System.out.println(" podałes nieprawidłowy typ liczbowy");
+//                System.out.println("pwiedz mi co dalej robić: koniec czy od nowa?");
+//                scanner.nextLine();
+//                String coDalej = scanner.nextLine();
+//                if (coDalej.equals("koniec".toLowerCase().trim())) {
+//                    System.out.println("koniec programu");
+//                   //break;
+//                } else if (coDalej.equals("od nowa".toLowerCase().trim())) {
+//
+//
+//
+//
+//                }
+//
+//            }
         //}
+//
+//
+//        try {
+//            for (int x : taqblica
+//                    ) {
+//                System.out.print(x + ", ");
+//            }
+//        } catch (ArrayIndexOutOfBoundsException ex) {
+//            System.out.println("wyswietalm błąd");
 
 
-        try {
-            for (int x : taqblica
-                    ) {
-                System.out.print(x + ", ");
+        //}
+        int licznik = scanner.nextInt();
+        int[] tablica = new int[2];
+        boolean flaga = true;
+while (flaga){
+            try {
+                for (int i = 0; i < licznik; i++) {
+                    int liczba = scanner.nextInt();
+                    tablica[i] = liczba;
+                    System.out.println(tablica[i]);
+
+                }
+
+                flaga = false;
+
+
+            } catch (Exception ex) {
+                System.err.println("zly typ liczbowy");
+                System.err.println("podales za duzo liczb");
+                ex.printStackTrace();
+            }finally {
+                System.out.println("koniec");
             }
-        } catch (ArrayIndexOutOfBoundsException ex) {
-            System.out.println("wyswietalm błąd");
-
-
+            //break;
         }
 
 
